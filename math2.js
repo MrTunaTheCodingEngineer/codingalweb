379199331d7894d2f747ffd103003e6ed8a7e2b8
MrTunaTheCodingEngineer <alitunapalta@icloud.com>
2025-02-03 15:17:00 +0100

    Add files via upload

---
+++ math2.js
@@ -0,0 +1,23 @@
+function addition(){
+
+
+    var a = 6;
+
+    var b = 4;
+
+    
+    document.getElementById("demo").innerHTML = a + b
+
+
+}
+
+function subtraction(){
+
+
+    var c = 18;
+
+    var d = 5;
+
+    document.getElementById("demo2").innerHTML = c - d
+
+}
